ceff597e91fbf7e607a7e62e7f452e0a73541d79
Alfonso Gomez <stein.hakase.vs@gmail.com>
2021-07-19 16:50:44 -0600

    🏷️ Added typings for env file

---
+++ src/types/env.types.ts
@@ -0,0 +1,24 @@
+export interface IEnv {
+  PORT: number
+  ADMIN_EMAIL: string
+  JWT_SECRET: string
+  REFRESH_JWT_SECRET: string
+  JWT_EXP: string
+  REFRESH_JWT_EXP: string
+  DB_URL: string
+  DOMAIN_URL: string
+  QUERY_COMPLEXITY_LIMIT: string
+  RATE_LIMIT_BAN_EXP: string
+  EMAIL_HOST: string
+  EMAIL_TLS_PORT: number
+  EMAIL_SSL_PORT: number
+  EMAIL_USER: string
+  EMAIL_PASS: string
+  EMAIL_VERIFICATION_EXP: string
+  EMAIL_PASSWORD_REQUEST_EXP: string
+  EMAIL_SECRET: string
+  ALLOWED_ORIGINS: string
+  SENTRY_DSN: string
+  SENTRY_SERVER_NAME: string
+  LOG_LEVEL: 'log' | 'error' | 'warn' | 'debug' | 'verbose'
+}
