f51610f9ee9016fc1d52a1f0bea8e6c7ef4c91d8
Alfonso Gomez <stein.hakase.vs@gmail.com>
2021-07-19 21:11:29 -0600

    ♻️ Refactored env validation to use class validator

--- src/env.validation.ts
+++ src/env.validation.ts
@@ -1,65 +1,61 @@
-import { ConfigModuleOptions } from '@nestjs/config'
-import * as Joi from 'joi'
+import { ConfigModule } from '@nestjs/config'
+import { plainToClass } from 'class-transformer'
+import { IsDefined, IsEmail, IsFQDN, IsIn, IsNumber, IsOptional, IsString, IsUrl, Min, MinLength, validateSync } from 'class-validator'
 
-console.log(process.env)
-
-export const envValidation: ConfigModuleOptions = {
-  validationSchema: Joi.object({
-    PORT: Joi.number()
-      .default(3002),
-    ADMIN_EMAIL: Joi.string()
-      .email()
-      .required(),
-    JWT_SECRET: Joi.string()
-      .min(30)
-      .required(),
-    REFRESH_JWT_SECRET: Joi.string()
-      .min(30)
-      .required(),
-    JWT_EXP: Joi.string()
-      .default('15m'),
-    REFRESH_JWT_EXP: Joi.string()
-      .default('1d'),
-    DB_URL: Joi.string()
-      .uri()
-      .required(),
-    DOMAIN_URL: Joi.string()
-      .uri()
-      .default('http://localhost'),
-    QUERY_COMPLEXITY_LIMIT: Joi.number()
-      .default(20),
-    RATE_LIMIT_BAN_EXP: Joi.string()
-      .default('1h'),
-    EMAIL_HOST: Joi.string()
-      .domain()
-      .default('smtp.gmail.com'),
-    EMAIL_TLS_PORT: Joi.number()
-      .default(587),
-    EMAIL_SSL_PORT: Joi.number()
-      .default(465),
-    EMAIL_USER: Joi.string()
-      .email()
-      .required(),
-    EMAIL_PASS: Joi.string()
-      .required(),
-    EMAIL_VERIFICATION_EXP: Joi.string()
-      .default('1w'),
-    EMAIL_PASSWORD_REQUEST_EXP: Joi.string()
-      .default('1d'),
-    EMAIL_SECRET: Joi.string()
-      .min(30),
-    ALLOWED_ORIGINS: Joi.string()
-      .default('*'),
-    SENTRY_DSN: Joi.string()
-      .uri(),
-    SENTRY_SERVER_NAME: Joi.string()
-      .alphanum(),
-    LOG_LEVEL: Joi.any()
-      .allow('log', 'error', 'warn', 'debug', 'verbose')
-      .default('verbose'),
-  }),
-  validationOptions: {
-    allowUnknown: false,
-    abortEarly: true
-  }
+export class EnvironmentVariables {
+  // GENERAL VARIABLES
+  @IsNumber() PORT: number = 3002
+  @IsDefined() @IsEmail() ADMIN_EMAIL: string
+  @IsUrl({ require_tld: false }) DOMAIN_URL: string = 'http://localhost'
+  @IsNumber() @Min(0) QUERY_COMPLEXITY_LIMIT: number = 20
+  @IsString() ALLOWED_ORIGINS: string = '*'
+  @IsIn(['log', 'error', 'warn', 'debug', 'verbose']) LOG_LEVEL: string = 'verbose'
+  // EXPIRATION VARIABLES
+  @IsString() JWT_EXP: string = '15m'
+  @IsString() REFRESH_JWT_EXP: string = '1d'
+  @IsString() RATE_LIMIT_BAN_EXP: string = '5h'
+  @IsString() EMAIL_VERIFICATION_EXP: string = '1w'
+  @IsString() EMAIL_PASSWORD_REQUEST_EXP: string = '1d'
+  // SECRETS
+  @IsDefined() @IsString() @MinLength(30) JWT_SECRET: string
+  @IsDefined() @IsString() @MinLength(30) REFRESH_JWT_SECRET: string
+  @IsDefined() @IsString() @MinLength(30) EMAIL_SECRET: string
+  // EMAIL VARIABLES
+  @IsString() @IsFQDN() EMAIL_HOST: string = 'smtp.gmail.com'
+  @IsNumber() EMAIL_TLS_PORT: number = 587
+  @IsNumber() EMAIL_SSL_PORT: number = 465
+  @IsDefined() @IsEmail() EMAIL_USER: string
+  @IsDefined() @IsString() EMAIL_PASS: string
+  // DB_VARIABLES
+  @IsUrl({ require_valid_protocol: false }) DB_URL: string
+  // SENTRY
+  @IsOptional()	@IsUrl() SENTRY_DSN: string
+  @IsOptional()	@IsString() SENTRY_SERVER_NAME: string
 }
+
+export function validateEnv (config: Record<string, unknown>) {
+  const validateConfig = plainToClass(
+    EnvironmentVariables,
+    config,
+    { enableImplicitConversion: true }
+  )
+
+  const errors = validateSync(
+    validateConfig,
+    {
+      skipMissingProperties: false,
+      stopAtFirstError: true,
+      forbidUnknownValues: true 
+    }
+  )
+
+  if (errors.length > 0) {
+    throw new Error(errors.toString())
+  }
+
+  return validateConfig
+}
+
+export const envValidation = ConfigModule.forRoot({
+  validate: validateEnv
+})
