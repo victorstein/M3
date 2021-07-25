5759bad0e7a056e9102ded464c12c18eabee7546
Alfonso Gomez <stein.hakase.vs@gmail.com>
2021-07-25 02:48:55 -0600

    ♻️ Configured exported members

--- src/role/role.module.ts
+++ src/role/role.module.ts
@@ -1,13 +1,13 @@
-import { Module } from '@nestjs/common';
+import { Logger, Module } from '@nestjs/common';
 import { RoleService } from './role.service';
 import { RoleResolver } from './role.resolver';
 import { Role } from './role.entity';
 import { MongooseModule, SchemaFactory } from '@nestjs/mongoose';
+import { Service } from 'src/base/service.base';
 
 @Module({
-  imports: [
-    MongooseModule.forFeature([{ name: 'Role', schema: SchemaFactory.createForClass(Role) }])
-  ],
-  providers: [RoleService, RoleResolver]
+  imports: [MongooseModule.forFeature([{ name: 'Role', schema: SchemaFactory.createForClass(Role) }])],
+  providers: [RoleService, RoleResolver, Service, Logger],
+  exports: [RoleService]
 })
 export class RoleModule {}
--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,11 +1,23 @@
-import { Module } from '@nestjs/common';
+import { Logger, Module } from '@nestjs/common';
 import { UserService } from './user.service';
 import { UserResolver } from './user.resolver';
 import { MongooseModule, SchemaFactory } from '@nestjs/mongoose';
 import { User } from './user.entity';
+import { Service } from 'src/base/service.base';
+// import { Role } from 'src/role/role.entity';
+import { RoleModule } from 'src/role/role.module';
 
 @Module({
-  imports: [MongooseModule.forFeature([{ name: 'User', schema: SchemaFactory.createForClass(User) }])],
-  providers: [UserService, UserResolver]
+  imports: [
+    MongooseModule.forFeature([{ name: 'User', schema: SchemaFactory.createForClass(User) }]),
+    RoleModule
+  ],
+  providers: [
+    Logger,
+    UserService,
+    UserResolver,
+    Service
+  ],
+  exports: [UserService]
 })
 export class UserModule {}
