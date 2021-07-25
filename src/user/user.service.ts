3b0e4f15cba70083d17462d4fd92ea5d6c5c5692
Alfonso Gomez <stein.hakase.vs@gmail.com>
2021-07-25 05:07:55 -0600

    ✨ Added new methods to userService

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,39 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable, Logger } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { RoleService } from 'role/role.service';
+import { Service } from 'base/service.base';
+import { Roles } from 'role/types/roleTypes';
+import { User } from './user.entity';
+import * as generator from 'generate-password'
+import * as argon2 from 'argon2'
+
 
 @Injectable()
-export class UserService {}
+export class UserService extends Service<User> {
+  @Inject() logger: Logger
+  @InjectModel('User') model: Model<User>
+  @Inject() roleService: RoleService
+
+  async findOneByRole (role: Roles): Promise<User | null> {
+    this.logger.verbose(`Operation: findOneByRole. \n role: ${role}`)
+    const foundRole = await this.roleService.findOneByParam({ type: role })
+    if (foundRole === null) return null
+    return await this.model.findOne({ role: foundRole._id })
+  }
+
+  generateTemporaryPassword (): string {
+    this.logger.verbose('Generating temporary password')
+    return generator.generate({
+      length: 30,
+      numbers: true,
+      strict: true,
+      symbols: true
+    })
+  }
+
+  async hashPassword (password: string): Promise<string> {
+    this.logger.verbose('Hashing temporary password')
+    return await argon2.hash(password, { type: argon2.argon2i })
+  }
+}
