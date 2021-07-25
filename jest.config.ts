dad88655930793915c887528d835c20c15c78c32
Alfonso Gomez <stein.hakase.vs@gmail.com>
2021-07-25 05:05:04 -0600

    🔧 Added jest configuration

---
+++ jest.config.ts
@@ -0,0 +1,19 @@
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  setupFilesAfterEnv: ["jest-extended"],
+  collectCoverage: true,
+  modulePaths: ['<rootDir>/src'],
+  displayName: {
+    name: 'M3',
+    color: 'cyan'
+  },
+  modulePathIgnorePatterns: [
+    'dist'
+  ],
+  silent: false,
+  collectCoverageFrom: [
+    "src/**/*.ts",
+    "!**/node_modules/**"
+  ]
+};
