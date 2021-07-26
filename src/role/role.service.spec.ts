d887ad813053654fd942564e5ab4a64e9ed548b3
Alfonso Gomez <stein.hakase.vs@gmail.com>
2021-07-25 19:22:20 -0600

    🧪 Fixed tests for role service

--- src/role/role.service.spec.ts
+++ src/role/role.service.spec.ts
@@ -1,12 +1,24 @@
-import { Test, TestingModule } from '@nestjs/testing';
+import { Role } from './role.entity';
 import { RoleService } from './role.service';
+import { Model } from 'mongoose';
+import { getModelToken } from '@nestjs/mongoose';
+import { Logger } from '@nestjs/common';
+import { Test, TestingModule } from '@nestjs/testing';
+import { mock } from 'jest-mock-extended';
+
+const mockRoleModel = mock<Model<Role>>()
+const mockLogger = mock<Logger>()
 
 describe('RoleService', () => {
   let service: RoleService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [RoleService],
+      providers: [
+        RoleService,
+        { provide: getModelToken('Role'), useValue: mockRoleModel },
+        { provide: Logger, useValue: mockLogger }
+      ],
     }).compile();
 
     service = module.get<RoleService>(RoleService);
