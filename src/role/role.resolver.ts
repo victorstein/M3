45b7ce11979e008fec9d058b95d779866e09bf3f
Alfonso Gomez <stein.hakase.vs@gmail.com>
2021-07-25 19:23:10 -0600

    🔥 Removed unnecessary dependencies for role resolver

--- src/role/role.resolver.ts
+++ src/role/role.resolver.ts
@@ -1,16 +1,9 @@
 import { Query, Resolver } from '@nestjs/graphql';
-import { InjectModel } from '@nestjs/mongoose';
-import { Model } from 'mongoose';
 import { Role } from './role.entity';
-import { Roles } from './types/roleTypes';
-
 @Resolver(() => Role)
 export class RoleResolver {
-  @InjectModel(Role.name) private readonly roleModel: Model<Role>
-
   @Query(() => String)
   async hello () {
-    await this.roleModel.create({ name: 'shit', description: 'shit 2', type: Roles.USER })
     return 'world'
   }
 }
