feddb7f11caa599576976313c459b325c57aa21b
Alfonso Gomez <stein.hakase.vs@gmail.com>
2021-09-12 20:26:40 -0600

    ✨ Added base module to app

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,11 +6,13 @@ import { EnvValidation } from './env.validation';
 import { graphqlConfig } from './graphql.config';
 import { UserModule } from './user/user.module';
 import { RoleModule } from './role/role.module';
+import { BaseModule } from 'base/base.module';
 @Module({
   imports: [
     EnvValidation,
     DBConnection,
     graphqlConfig,
+    BaseModule,
     RoleModule,
     UserModule
   ],
