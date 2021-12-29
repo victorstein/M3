52b365eb6578877caa4062dd7ae9c1f14fabc389
Alfonso Gomez <stein.hakase.vs@gmail.com>
2021-12-29 00:22:56 -0600

    ✨ Updated login args to support multiple login paradigms

--- src/auth/dto/login.args.ts
+++ src/auth/dto/login.args.ts
@@ -1,13 +1,37 @@
-import { ArgsType, Field } from '@nestjs/graphql'
-import { IsEmail, IsString } from 'class-validator'
+import { ArgsType, Field, registerEnumType } from '@nestjs/graphql'
+import { AuthTypes } from 'auth/auth.types'
+import { IsEmail, IsOptional, IsString } from 'class-validator'
+
+registerEnumType(
+  AuthTypes,
+  {
+    name: 'AuthTypes',
+    description: 'Authentication types used to grant access to users',
+    valuesMap: {
+      EMAIL_AND_PASSWORD: {
+        description: 'Default value for Auth types'
+      }
+    }
+  }
+)
 
 @ArgsType()
 export class LoginArgs {
+  @Field(() => AuthTypes, { nullable: true })
+  authType: AuthTypes = AuthTypes.EMAIL_AND_PASSWORD
+
+  @IsOptional()
   @IsEmail({}, { message: 'The provided email is invalid' })
-  @Field({ nullable: false })
+  @Field({ nullable: true })
   email: string
 
-  @Field({ nullable: false })
+  @IsOptional()
   @IsString()
+  @Field({ nullable: true })
   password: string
+
+  @IsOptional()
+  @IsString()
+  @Field({ nullable: true })
+  token: string
 }
