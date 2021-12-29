4a0aeef57938b0467b4717a084e6c433e9e0f56d
Alfonso Gomez <stein.hakase.vs@gmail.com>
2021-12-29 00:21:09 -0600

    🏷️ Added new types in the auth module

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,10 +3,21 @@ import { UserModule } from 'user/user.module'
 import { AuthResolver } from './auth.resolver'
 import { AuthService } from './auth.service'
 import { JWTCookieStrategy } from './authStrategies/jwt-coockie.strategy'
+import { EmailAndPasswordAuthStrategy } from './loginStrategies/emailAndPassword.strategy'
+import { GoogleAuthStrategy } from './loginStrategies/google.strategy'
+import { LoginFactory } from './loginStrategies/loginFactory'
 
 @Module({
   imports: [UserModule],
-  providers: [AuthResolver, AuthService, Logger, JWTCookieStrategy],
+  providers: [
+    AuthResolver,
+    AuthService,
+    Logger,
+    JWTCookieStrategy,
+    LoginFactory,
+    GoogleAuthStrategy,
+    EmailAndPasswordAuthStrategy
+  ],
   exports: [AuthService]
 })
 export class AuthModule {}
