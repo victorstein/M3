280f5cfbd858855b1d6704fe2f5ec3d9da4b1d4e
Alfonso Gomez <stein.hakase.vs@gmail.com>
2022-01-02 16:41:13 -0600

    ♻️ Refactored auth resolver to handle mobile login

--- src/auth/auth.resolver.ts
+++ src/auth/auth.resolver.ts
@@ -1,10 +1,11 @@
 import { Inject, Injectable } from '@nestjs/common'
 import { Resolver, Query, Args, Context } from '@nestjs/graphql'
-import { LoginArgs } from './dto/login.args'
+import { LoginArgs } from './dto/login.input.dto'
 import { AuthService } from './auth.service'
 import { IContext } from 'app.types'
-import { CookieNames, cookieOptions, IPayload } from './auth.types'
+import { CookieNames, cookieOptions, TokenType } from './auth.types'
 import { UserService } from 'user/user.service'
+import { isMobile } from 'common/utils'
 
 @Resolver()
 @Injectable()
@@ -12,20 +13,30 @@ export class AuthResolver {
   @Inject() authService: AuthService
   @Inject() userService: UserService
 
-  @Query(() => String)
-  async login (@Args() loginArgs: LoginArgs, @Context() { res }: IContext): Promise<string> {
-    const user = await this.authService.validateUser(loginArgs)
+  @Query(() => String, { description: 'Query used to login the user, the backend will determine if the request is mobile or desktop' })
+  async login (@Args() loginArgs: LoginArgs, @Context() { req, res }: IContext): Promise<string> {
+    const user = await this.authService.authenticateUser(loginArgs)
+
+    // Check if the requester is a mobile device not in a browser
+    const mobile = isMobile(req)
+    const tokenType = mobile ? TokenType.MOBILE : TokenType.REFRESH
+    const iat = mobile ? 'lastSignedMobile' : 'lastSigned'
 
     const token = this.authService.generateToken({ user })
-    const refreshToken = this.authService.generateToken({ user, isRefreshToken: true })
+    const refreshToken = this.authService.generateToken({ user, tokenType })
 
     // Get the new Issued at date
-    const newIat = this.authService.validateToken({ token: refreshToken, isRefreshToken: true }) as IPayload
+    const newIat = this.authService.validateToken({ token: refreshToken, tokenType })
     // Save the new Iat unix timestamp in the user for refresh token validation
-    await this.userService.updateOneById(newIat.userId, { lastSigned: newIat.iat })
+    await this.userService.updateOneById(newIat.userId, { [iat]: newIat.iat })
 
-    res.cookie(CookieNames.JWT, token, cookieOptions)
-    res.cookie(CookieNames.REFRESH, refreshToken, cookieOptions)
+    if (mobile) {
+      res.setHeader(CookieNames.JWT, token)
+      res.setHeader(CookieNames.REFRESH, refreshToken)
+    } else {
+      res.cookie(CookieNames.JWT, token, cookieOptions)
+      res.cookie(CookieNames.REFRESH, refreshToken, cookieOptions)
+    }
 
     return 'login successful'
   }
