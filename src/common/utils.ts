ea955fa69a466b7cf1cb38421b4f0714e92934af
Alfonso Gomez <stein.hakase.vs@gmail.com>
2022-01-02 16:43:52 -0600

    ✨ Created utils file to store reusable functions

---
+++ src/common/utils.ts
@@ -0,0 +1,13 @@
+import { Request } from 'express'
+import * as userAgent from 'express-useragent'
+
+export const isMobile = (req: Request): boolean => {
+  const ua = userAgent.parse(req.headers['user-agent'] ?? '')
+  console.log(req.headers['user-agent'])
+
+  // Determine if the user is on mobile or desktop
+  const isMobile = ua.isMobile
+  const isNotBrowser = ua.browser === 'unknown'
+
+  return isMobile && isNotBrowser
+}
