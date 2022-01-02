45305029854f5753c1c197059668792bdc1245eb
Alfonso Gomez <stein.hakase.vs@gmail.com>
2022-01-02 16:42:41 -0600

    ♻️ Refactored cookie strategy to handle mobile devices and use new auth service

--- src/auth/authStrategies/jwt-coockie.strategy.ts
+++ src/auth/authStrategies/jwt-coockie.strategy.ts
@@ -1,10 +1,11 @@
 import { Inject, Injectable, Logger } from '@nestjs/common'
 import { PassportStrategy } from '@nestjs/passport'
 import { Strategy } from 'passport-custom'
-import { AuthStrategies, CookieNames, cookieOptions, IPayload, JWTErrorTypes } from '../auth.types'
+import { AuthStrategies, CookieNames, cookieOptions, IPayload, JWTErrorTypes, TokenType } from '../auth.types'
 import { Request } from 'express'
 import { AuthService } from 'auth/auth.service'
 import { UserService } from 'user/user.service'
+import { isMobile } from 'common/utils'
 
 @Injectable()
 export class JWTCookieStrategy extends PassportStrategy(Strategy, AuthStrategies.JWT_COOKIE) {
@@ -12,8 +13,23 @@ export class JWTCookieStrategy extends PassportStrategy(Strategy, AuthStrategies
   @Inject() private readonly userService: UserService
   @Inject() private readonly logger: Logger
 
+  async checkIfRefreshable ({ name, message }: Error, req: Request): Promise<boolean> {
+    if (name === JWTErrorTypes.EXPIRED) {
+      return await this.refreshTokens(req)
+    } else {
+      this.logger.error(message)
+      return false
+    }
+  }
+
+  getTokens (req: Request): { jwt: string, refresh: string } {
+    const credsSource = isMobile(req) ? req.headers : req.signedCookies
+    const { jwt, refresh } = credsSource
+    return { jwt, refresh }
+  }
+
   async validate (req: Request): Promise<boolean> {
-    const { jwt, refresh } = req.signedCookies
+    const { jwt, refresh } = this.getTokens(req)
 
     // Check if both headers come in the request
     if (jwt === undefined || refresh === undefined) {
@@ -21,18 +37,12 @@ export class JWTCookieStrategy extends PassportStrategy(Strategy, AuthStrategies
       return false
     }
 
-    // Check if JWT is valid
-    const payload = this.authService.validateToken({ token: jwt })
+    let payload: IPayload
 
-    // If any error happened but expiration
-    if (payload === null) {
-      this.logger.verbose('JWT error')
-      return false
-    }
-
-    // If token expired refresh the token
-    if (payload === JWTErrorTypes.EXPIRED) {
-      return await this.refreshTokens(req)
+    try {
+      payload = this.authService.validateToken({ token: jwt })
+    } catch (err) {
+      return await this.checkIfRefreshable(err, req)
     }
 
     // If token did not expire validate the token version
@@ -41,6 +51,7 @@ export class JWTCookieStrategy extends PassportStrategy(Strategy, AuthStrategies
       this.logger.error('User in payload is invalid')
       return false
     }
+
     if (user.tokenVersion !== payload.version) {
       this.logger.error('Failed token version validation')
       return false
@@ -50,17 +61,23 @@ export class JWTCookieStrategy extends PassportStrategy(Strategy, AuthStrategies
   }
 
   async refreshTokens (req: Request): Promise<boolean> {
-    const { refresh } = req.signedCookies
-    // validate refresh token authenticity
-    const refreshPayload = this.authService.validateToken({ token: refresh, isRefreshToken: true }) as IPayload
+    const { refresh } = this.getTokens(req)
+    const mobile = isMobile(req)
 
-    // If any error happened dont authorize
-    if (refreshPayload === null) {
-      this.logger.error('Refresh token error')
+    const tokenType = mobile ? TokenType.MOBILE : TokenType.REFRESH
+    const iat = mobile ? 'lastSignedMobile' : 'lastSigned'
+
+    let refreshPayload
+    // validate refresh token authenticity
+    try {
+      refreshPayload = this.authService.validateToken({ token: refresh, tokenType })
+    } catch (err) {
+      this.logger.error(`Refresh token: ${String(err.message)}`)
       return false
     }
 
     const user = await this.userService.findById(refreshPayload.userId)
+
     if (user === null) {
       this.logger.error('User in payload is invalid')
       return false
@@ -73,8 +90,9 @@ export class JWTCookieStrategy extends PassportStrategy(Strategy, AuthStrategies
     }
 
     // Validate that this is the latest signed token
-    if (refreshPayload.iat !== user.lastSigned) {
+    if (refreshPayload.iat !== user[iat]) {
       this.logger.error('Failed IAT validation')
+      // If failed IAT validation revoke access to all tokens
       await this.userService.updateOneById(refreshPayload.userId, { tokenVersion: user.tokenVersion + 1 })
       return false
     }
@@ -82,15 +100,24 @@ export class JWTCookieStrategy extends PassportStrategy(Strategy, AuthStrategies
     this.logger.verbose('Renewing jwt and refresh token')
     // If the refresh token didn't error out then generate a new token and refresh token
     const newJwt = this.authService.generateToken({ user })
-    const newRefresh = this.authService.generateToken({ user, isRefreshToken: true })
+    const newRefresh = this.authService.generateToken({ user, tokenType })
 
     // Get the new Issued at date
-    const newIat = this.authService.validateToken({ token: newRefresh, isRefreshToken: true }) as IPayload
+    const newIat = this.authService.validateToken({ token: newRefresh, tokenType })
     // Save the new Iat unix timestamp in the user for refresh token validation
-    await this.userService.updateOneById(newIat.userId, { lastSigned: newIat.iat })
+    await this.userService.updateOneById(newIat.userId, { [iat]: newIat.iat })
 
-    req.res?.cookie(CookieNames.JWT, newJwt, cookieOptions)
-    req.res?.cookie(CookieNames.REFRESH, newRefresh, cookieOptions)
+    console.log('ISMOBILE', mobile)
+    if (mobile) {
+      console.log('REQUEST HEADER', req.res?.setHeader)
+      console.log('NEW JWT', newJwt)
+      console.log('NEW REFRESH', newRefresh)
+      req.res?.setHeader(CookieNames.JWT, newJwt)
+      req.res?.setHeader(CookieNames.REFRESH, newRefresh)
+    } else {
+      req.res?.cookie(CookieNames.JWT, newJwt, cookieOptions)
+      req.res?.cookie(CookieNames.REFRESH, newRefresh, cookieOptions)
+    }
 
     return true
   }
