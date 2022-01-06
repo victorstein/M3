2d46891289675a854eb9acc110669196d9373531
Alfonso Gomez <stein.hakase.vs@gmail.com>
2022-01-06 01:09:54 -0600

    ✨ Added new dependencies to seeder module

--- src/seeder/seeder.module.ts
+++ src/seeder/seeder.module.ts
@@ -1,17 +1,26 @@
 import { Logger, Module } from '@nestjs/common'
-import { DBConnection } from 'db.config'
-import { EnvValidation } from 'env.validation'
+import { DBConnection } from 'config/db.config'
+import { EnvValidation } from 'config/env.validation'
 import { UserModule } from 'user/user.module'
 import { RoleModule } from 'role/role.module'
 import { Seeder } from './seeder'
 import { SeederService } from './seeder.service'
-
+import { QueueModule } from 'queue/queue.module'
+import { AuthModule } from 'auth/auth.module'
+import { EmailModule } from 'email/email.module'
+import { EmailConfig } from 'config/email.config'
+import { BullConfig } from 'config/queue.config'
 @Module({
   imports: [
     EnvValidation,
     DBConnection,
+    BullConfig,
+    EmailConfig,
     RoleModule,
-    UserModule
+    UserModule,
+    QueueModule,
+    AuthModule,
+    EmailModule
   ],
   providers: [Logger, SeederService, Seeder]
 })
