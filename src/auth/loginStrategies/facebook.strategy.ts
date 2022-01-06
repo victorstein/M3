17b68075dc956b71fc849b2b1439ca589dc03a1f
Alfonso Gomez <stein.hakase.vs@gmail.com>
2022-01-06 01:08:52 -0600

    ♻️ Refactor facebook strategy to use http module

--- src/auth/loginStrategies/facebook.strategy.ts
+++ src/auth/loginStrategies/facebook.strategy.ts
@@ -1,9 +1,10 @@
+import { HttpService } from '@nestjs/axios'
 import { Injectable, InternalServerErrorException, Logger, UnauthorizedException } from '@nestjs/common'
 import { ConfigService } from '@nestjs/config'
 import { DocumentType } from '@typegoose/typegoose'
 import { Credentials, FacebookUser, ILogin } from 'auth/auth.types'
-import Axios from 'axios'
 import { IEnv } from 'env.types'
+import { lastValueFrom, map } from 'rxjs'
 import { User } from 'user/user.entity'
 import { UserService } from 'user/user.service'
 import { SocialLogin } from './social.abstract'
@@ -15,7 +16,8 @@ export class FacebookAuthStrategy extends SocialLogin implements ILogin {
   constructor (
     private readonly logger: Logger,
     private readonly userService: UserService,
-    private readonly configService: ConfigService<IEnv>
+    private readonly configService: ConfigService<IEnv>,
+    private readonly httpService: HttpService
   ) {
     super()
     this.jwk_uri = this.configService.get('FACEBOOK_GRAPH_URI', '')
@@ -23,12 +25,17 @@ export class FacebookAuthStrategy extends SocialLogin implements ILogin {
 
   async validateToken (token: string): Promise<FacebookUser> {
     try {
-      const { data } = await Axios.get<FacebookUser>(`${this.jwk_uri}/me`, {
-        params: {
-          fields: 'id,name,email',
-          access_token: token
-        }
-      })
+      const params = {
+        fields: 'id,name,email',
+        access_token: token
+      }
+
+      const observable = this.httpService
+        .get<FacebookUser>(`${this.jwk_uri}/me`, { params })
+        .pipe(map(resp => resp.data))
+
+      const data = await lastValueFrom(observable)
+
       return data
     } catch (error) {
       this.logger.error(error.message)
