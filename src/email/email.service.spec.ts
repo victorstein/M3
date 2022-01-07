62120a231850ac88cc6ca7a02abca53b5cef2be4
Alfonso Gomez <stein.hakase.vs@gmail.com>
2022-01-06 20:42:30 -0600

    ✅ Fixed failing tests

--- src/email/email.service.spec.ts
+++ src/email/email.service.spec.ts
@@ -1,12 +1,27 @@
+import { MailerService } from '@nestjs-modules/mailer'
+import { Logger } from '@nestjs/common'
+import { ConfigService } from '@nestjs/config'
 import { Test, TestingModule } from '@nestjs/testing'
+import { TokenFactory } from 'auth/tokenStrategies/token.factory'
+import { mock } from 'jest-mock-extended'
 import { EmailService } from './email.service'
 
 describe('EmailService', () => {
   let service: EmailService
+  const mockTokenFactory = mock<TokenFactory>()
+  const mockMailerService = mock<MailerService>()
+  const mockConfigService = mock<ConfigService>()
+  const mockLogger = mock<Logger>()
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [EmailService]
+      providers: [
+        EmailService,
+        { provide: TokenFactory, useValue: mockTokenFactory },
+        { provide: MailerService, useValue: mockMailerService },
+        { provide: Logger, useValue: mockLogger },
+        { provide: ConfigService, useValue: mockConfigService }
+      ]
     }).compile()
 
     service = module.get<EmailService>(EmailService)
--- src/seeder/seeder.service.spec.ts
+++ src/seeder/seeder.service.spec.ts
@@ -4,6 +4,7 @@ import { Test, TestingModule } from '@nestjs/testing'
 import { DocumentType } from '@typegoose/typegoose'
 import { IEnv } from 'env.types'
 import { mock } from 'jest-mock-extended'
+import { EmailProducer } from 'queue/email/email.producer'
 import { Role } from 'role/role.entity'
 import { RoleService } from 'role/role.service'
 import { User } from 'user/user.entity'
@@ -14,6 +15,7 @@ const mockUserService = mock<UserService>()
 const mockRoleService = mock<RoleService>()
 const mockLogger = mock<Logger>()
 const mockConfigService = mock<ConfigService<IEnv>>()
+const mockEmailProducer = mock<EmailProducer>()
 
 jest.mock('role/types/role.types', () => ({
   __esModule: true,
@@ -30,6 +32,7 @@ describe('SeederService', () => {
         { provide: UserService, useValue: mockUserService },
         { provide: RoleService, useValue: mockRoleService },
         { provide: Logger, useValue: mockLogger },
+        { provide: EmailProducer, useValue: mockEmailProducer },
         { provide: ConfigService, useValue: mockConfigService }
       ]
     }).compile()
@@ -41,6 +44,7 @@ describe('SeederService', () => {
     jest.clearAllMocks()
     // Reset default value for the mocks
     mockUserService.findOneByRole.mockResolvedValue({} as const as DocumentType<User>)
+    mockUserService.create.mockResolvedValue({ id: 'createdUser' } as const as DocumentType<User>)
     mockRoleService.findOneByParam.mockResolvedValue({ _id: 'foundRole' } as const as DocumentType<Role>)
     mockConfigService.get.mockReturnValue('test@test.test')
   })
